d1b9368c83b4bf4ed9f1d1f4da001027b1a0078f
ccheung8 <c4cheung@ucsd.edu>
2023-03-14 18:20:00 -0700

    added files

---
+++ index.js
@@ -0,0 +1,19 @@
+const left = document.getElementById("left-side");
+const hero = document.getElementById("hero");
+const nav = document.getElementsByTagName("nav");
+
+console.log(nav[0]);
+
+// hero on home page
+const handleOnMove = e => {
+  // calcs width of window as percentage based on mouse pos
+  const p = e.clientX / window.innerWidth * 100;
+
+  // assigns to width attribute
+  left.style.width = `${p}%`;
+}
+
+// binds mouse move listeners
+hero.onmousemove = e => handleOnMove(e);
+// on touch for touchscreens
+hero.ontouchmove = e => handleOnMove(e.touches[0]);
